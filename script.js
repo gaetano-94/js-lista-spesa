6f397ee355c22981f369c45063d8f4cca00e6861
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-13 17:58:02 +0100

    feat: creo degli li con js

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ const ulLista = document.getElementById('listaSpesa');
 
 // ARRAY CON GLI ELEMENTI DELLA SPESA
 const elementiLista = [
-  'pane',
-  'latte',
-  'pasta',
-  'uova',
-  'burro',
-  'farina',
-  'riso',
-  'cereali',
-  'zucchero',
-  'caffe',
-  'miele',
+  'Pane',
+  'Latte',
+  'Pasta',
+  'Uova',
+  'Burro',
+  'Farina',
+  'Riso',
+  'Cereali',
+  'Zucchero',
+  'Caffe',
+  'Miele',
 ];
+
+// indice del ciclo
+let i = 0;
+
+//creo un lista con while
+
+while (i < elementiLista.length) {
+  let liLista = document.createElement('li');
+
+  //collegare la mia array all'elemento li
+  liLista.innerText = elementiLista[i];
+
+  //aggiungere li al DOM
+  ulLista.appendChild(liLista);
+  i++;
+}
