fbf590911d0c67765b85a312088806a0c68bbd4f
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-13 17:37:32 +0100

    feat: elementi array

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const ulLista = document.getElementById('listaSpesa');
+
+// ARRAY CON GLI ELEMENTI DELLA SPESA
+const elementiLista = [
+  'pane',
+  'latte',
+  'pasta',
+  'uova',
+  'burro',
+  'farina',
+  'riso',
+  'cereali',
+  'zucchero',
+  'caffe',
+  'miele',
+];
